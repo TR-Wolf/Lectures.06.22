479557e8eabb554b69c778a07e4d376d54838ebb
Jay M <jaymansmann@yahoo.com>
2022-06-30 10:16:52 -0400

    AbstractClasses lecture code

--- week2/AbstractClasses.java
+++ week2/AbstractClasses.java
@@ -1,5 +1,10 @@
 public class AbstractClasses {
     public static void main(String[] args) {
+        CheckingAccount account = new CheckingAccount();
+        System.out.println(account.deposit(10_000));
+        System.out.println(account.deposit(10_000));
+        System.out.println(account.deposit(10_000));
+
         // Make an AbstractBankAccount
         // Make a CheckingAccount that extends AbstractAccount
         // Make a SavingsAccount that extends AbstractAccount
@@ -10,14 +15,32 @@ public class AbstractClasses {
 }
 
 abstract class AbstractAccount {
+    private double balance;
     public AbstractAccount() {
-
+        this.balance = 0;
     }
-
-    // Methods???
-
+    public double deposit(double amount) {
+        this.balance += amount;
+        return this.balance;
+    }
+    abstract public double withdraw(double amount);
+    public double getBalance() {
+        return balance;
+    }
 }
-
+class CheckingAccount extends AbstractAccount {
+    // @Override
+    public double deposit(double amount) {
+        double result = super.deposit(amount);
+        // any other special code for checking accounts
+        return result;
+    }
+    // @Override
+    public double withdraw(double amount) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
 
 
 // Now what if you need to add interest to the account?
