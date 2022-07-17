22720e1b8ef67bb2955e374f42c9b0df0b4cd1c9
init.defaultBranch <thomasrussellwolf@gmail.com>
2022-07-16 18:59:11 -0600

    two more datastructures

--- week5/collectionsRunner.java
+++ week5/collectionsRunner.java
@@ -52,21 +52,88 @@ void    clear(): Removes all elements from the collection.
 //
 //            myList.forEach((s) -> System.out.println(s));
 
-        ////Priority Queue
-        PriorityQueue<Person> myQueue = new PriorityQueue<Person>();
-        }
+        ////Priority Queue (goto Person first)
+//        PriorityQueue<Person> emergencyRoomQueue = new PriorityQueue<>();
+//
+//        emergencyRoomQueue.add(new Person("Caspoor", -5));
+//        emergencyRoomQueue.add(new Person("Joe", 4));
+//        emergencyRoomQueue.add(new Person("George", 4));
+//
+//        //Who get's to leave first?
+//        Person personInNeed = emergencyRoomQueue.remove();
+//        personInNeed.admit();
+//
+//        //and second?
+//        personInNeed = emergencyRoomQueue.remove();
+//        personInNeed.admit();
+
+        ////Let's add a few billionaires, and Thomas
+//        emergencyRoomQueue.add(new Person("Jeffrey Bezos", (136.3 * Math.pow(10, 6)))); // 136 Billion
+//        emergencyRoomQueue.add(new Person("Elon Musk", (229.1 * Math.pow(10, 6)))); //229 billion
+//        emergencyRoomQueue.add(new Person("Thomas Wolf", 20));
+//        System.out.println("Billionaires (and Thomas) have walked into the ER waiting room.");
+//
+//        personInNeed = emergencyRoomQueue.remove();
+//        personInNeed.admit(); //who?
+//
+//        personInNeed = emergencyRoomQueue.remove();
+//        personInNeed.admit();
+//
+//
+//        personInNeed = emergencyRoomQueue.remove();
+//        personInNeed.admit();
+
+        //// Tree Set
+        TreeSet<Person> hospitalPatientsSet = new TreeSet<>();
+
+        //let's use the same 6 patients
+        hospitalPatientsSet.add(new Person("Caspoor", -5));
+        hospitalPatientsSet.add(new Person("Joe", 4));
+        hospitalPatientsSet.add(new Person("George", 4));
+        hospitalPatientsSet.add(new Person("Jeffrey Bezos", (136.3 * Math.pow(10, 6)))); // 136 Billion
+        hospitalPatientsSet.add(new Person("Elon Musk", (229.1 * Math.pow(10, 6)))); //229 billion
+        hospitalPatientsSet.add(new Person("Thomas Wolf", 20));
+
+        Person first = hospitalPatientsSet.first();
+        System.out.println(first.toString() + " will be seen first today.");
+
+        Person last = hospitalPatientsSet.last();
+        System.out.println(last.toString() + " will be seen last today.");
+
+        //// Let's try: range(l,u), headset(#) and tailset(#) (smaller and larger)
 
 
-    class Person {
+
+    }
+
+
+    static class Person implements Comparable {
         private String name;
-        private int value;
+        //my Networth in thousands
+        private double netWorth;
 
-        Person(String name, int value){
+        Person(String name, double netWorth){
             this.name = name;
-            this.value = value;
+            this.netWorth = netWorth;
         }
-        public int getValue() {
-            return this.value;
+        public double getNetWorth() {
+            return this.netWorth;
+        }
+        public void admit(){
+            System.out.println(this.name + " is to admitted to the ER.");
+        }
+
+        @Override
+        public String toString() {
+            return this.name;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            // We want to consider this as a person, not as an object.
+            Person otherPerson = (Person)o;
+            // Do I have a larger net worth? How much bigger is my net worth?
+            return (int) (otherPerson.getNetWorth() - this.netWorth);
         }
     }
     
