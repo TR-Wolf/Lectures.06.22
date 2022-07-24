c10c432d02df17fa20ba29621cb5311a8dd6e849
init.defaultBranch <thomasrussellwolf@gmail.com>
2022-07-24 00:18:50 -0600

    thread basics start

--- week6/ThreadBasics.java
+++ week6/ThreadBasics.java
@@ -1,2 +1,29 @@
 public class ThreadBasics {
+    public static void main(String[] args) throws InterruptedException {
+        Chef spongeBob = new Chef();
+        final Boolean[] burgerCooked = {false};
+
+        Thread chefThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                spongeBob.fryBurger();
+                burgerCooked[0] = true;
+            }
+        });
+
+        log("Okay how can I take your order?");
+        log("burger? okay got it. SPONGEBOB!!!!");
+        chefThread.start();
+        while (!burgerCooked[0]) {
+            System.out.println("... waiting...");
+            Thread.sleep(2000);
+        }
+        chefThread.join();
+
+        log("Your burger is ready!!!!! Enjoy!");
+    }
+
+    private static void log(String s){
+        System.out.println("Squidward: \"" + s + "\"");
+    }
 }
